ca1f584d29027234a12880457c8fbf938cacafff
Sébastien Dagnicourt <sebastien.dagnicourt@gmail.com>
2018-08-17 22:42:44 +0200

    sort by genre ok

--- sort_bygenre.py
+++ sort_bygenre.py
@@ -4,24 +4,41 @@ import os
 import sys
 import errno
 import mutagen
-from shutil import copyfile
-
+from shutil import copyfile, move
+from pathlib import Path
 from mutagen.easyid3 import EasyID3
 
 
 dir = sys.argv[1]
 
-def cdir(dname):
+def mdir(dname):
   try:
-    os.mkdir(dir+'/'+dname)
+    os.mkdir(dname)
   except OSError as exc:
     if exc.errno != errno.EEXIST:
+      print(dname)
       raise
     pass
 
 def getdgenre(artist,dname):
+  artist = artist.replace('/','')
   p = dname.replace(artist,'')
   return os.path.basename(os.path.normpath(p))
+
+
+def movefile(src,dst,genre,artist,file):
+  artist = artist.replace('/','')
+  dgpath = dst+'/'+genre
+  dpath = dgpath+'/'+artist
+  dfile = dpath+'/'+file
+  mdir(dgpath)
+  mdir(dpath)
+  my_file = Path(dfile)
+  if my_file.exists():
+    print("ERROR: File "+dfile+" already exists (from:"+src+")")
+  else:
+    print("moving "+src+" to "+dfile)
+    move(src,dfile)
   
 for root, dirs, files in os.walk(dir):
   for file in files:
@@ -29,5 +46,8 @@ for root, dirs, files in os.walk(dir):
       filename = root+'/'+file
       info = mutagen.File(filename, easy=True)
       dgenre = getdgenre(info['artist'][0],root)
-      if ( info['genre'][0] != dgenre ):
-        print(filename+"##"+genre+"##"+dgenre)
+      tgenre = info['genre'][0].replace('/','')
+      if ( tgenre != dgenre ):
+        print("tgenre: "+tgenre+", dgenre: "+dgenre+", artist: "+info['artist'][0])
+        movefile(filename,dir,tgenre,info['artist'][0],file)
+ 
