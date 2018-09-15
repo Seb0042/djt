0ee7c15faad3be770d3cc3e2027c6410c90eaa55
Sébastien Dagnicourt <sebastien.dagnicourt@gmail.com>
2018-09-15 18:51:02 +0200

    xspf ok

--- create_lists.py
+++ create_lists.py
@@ -4,18 +4,19 @@ import os
 import sqlite3
 import sys
 import xspf
-
+from xml.etree import ElementTree
 from mutagen.easyid3 import EasyID3
+
 conn = sqlite3.connect('songs.db')
 c = conn.cursor()
-
 sql_genre = 'select distinct genre from songs order by genre;'
+
 for row in c.execute(sql_genre):
   genre = row[0]
   t = (row[0],)
   
   cg = conn.cursor()
-  radio_file = open('Target/'+genre.replace('/','')+'/'+'radio.xml', 'w',1,encoding='utf-8')
+  radio_file = open('Target/'+genre.replace('/','')+'/'+'radio.xml', 'w')
   sql_radio = "select filepath,filename,title,artist from songs where genre = ? and ( com = 'Mix' or com = 'Listen' or com = 'Radio' )"
   x = xspf.Xspf()
   x.title = "LetsDance "+genre+" playlist"
@@ -23,14 +24,14 @@ for row in c.execute(sql_genre):
     title = row_radio[2]
     creator = row_radio[3]
     location = row_radio[0].replace('Target/','oc://')+'/'+row_radio[1]
-    x.add_track({"title": title, "creator": creator, "location": location})
-  xmldata = tostring(x.toXml ,encoding="unicode")
-  print( xmldata)
-  radio_file.write( xml)
+    x.add_track({"title": title, "creator": creator, "location": location})	
+  d = x.toXml().decode('utf-8')
+#  xmldata = ElementTree.tostring(d,encoding="unicode")
+  print( d )
+  radio_file.write( d )
   cg.close()
   radio_file.close()
 
-  
   cg = conn.cursor()
   listen_file = open('Target/'+genre.replace('/','')+'/'+'listen.txt', 'w')
   sql_listen = "select filepath,filename from songs where genre = ? and ( com = 'Mix' or com = 'Listen' )"
